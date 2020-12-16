f6282e6c8a8fa0c12ca8b5580e0cfaff19d3160d
adi <adipeled@gmail.com>
2020-12-16 12:08:08 +0200

    kk

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    publicPath: 'portfolio',
+    chainWebpack: config => {
+        config.module.rules.delete("svg");
+    },
+    configureWebpack: {
+        module: {
+            rules: [
+                {
+                    test: /\.svg$/,
+                    loader: 'vue-svg-loader',
+                },
+            ],
+        }
+    }
+};
