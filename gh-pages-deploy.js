878dd81b2551d97fca3b2f690e90bdb6d0728bd9
adi <adipeled@gmail.com>
2020-12-21 14:37:50 +0200

    footer

---
+++ gh-pages-deploy.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+const execa = require("execa");
+const fs = require("fs");
+(async () => {
+  try {
+    await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    // eslint-disable-next-line no-console
+    console.log("Building started555...");
+    await execa("npm", ["run", "build"]);
+    // Understand if it's dist or build folder
+    const folderName = fs.existsSync("dist") ? "dist" : "build";
+    await execa("git", ["--work-tree", folderName, "add", "--all"]);
+    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+    console.log("Pushing to gh-pages...1");
+    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+    console.log("Pushing to gh-pages..2.");
+    await execa("rm", ["-r", folderName]);
+    console.log("Pushing to gh-pages.3..");
+    await execa("git", ["checkout", "-f", "master"]);
+    console.log("Pushing to gh-pages.4..");
+    await execa("git", ["branch", "-D", "gh-pages"]);
+    console.log("Successfully deployed, check your settings");
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e.message);
+    process.exit(1);
+  }
+})();
---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,61 @@
+<template>
+  <footer class="footer" id="contact ">
+    <div class="contact">
+      <h3 class="footer__title">Contact me</h3>
+      <a
+        class="footer__link"
+        target="_blank"
+        href="https://mail.google.com/mail/?view=cm&fs=1&to=adipeled499@gmail.com&su=&body=text here "
+      >
+        <EmailSvg style="{style.svgFirst}" className="svg"></EmailSvg>
+        <label style="{style.labelFirst}">
+          Email Me: adipeled499@gmail.com</label
+        >
+      </a>
+
+      <a
+        class="footer__link"
+        target="_blank"
+        href="https://api.whatsapp.com/send?phone=972556699584"
+      >
+        <WhatsappSvg style="{style.svgSecond}" className="svg"></WhatsappSvg>
+
+        <label style="{style.labelSecond}">
+          Send Whatsapp: 055-6699584</label
+        ></a
+      >
+    </div>
+    <p class="footer__copyright">
+      Built by Adi Peled Copyright &copy; by jonas Schmedtmann
+    </p>
+    <ul class="flex">
+      <li>
+        <Facebook class="svg" />
+      </li>
+      <li>
+        <Instagram class="svg" />
+      </li>
+      <li>
+        <Linkedin class="svg" />
+      </li>
+    </ul>
+  </footer>
+</template>
+
+<script>
+import "./Footer.scss";
+// import Facebook from "../../assets/img/social/facebook.svg";
+import Facebook from "@/assets/img/social/facebook.svg";
+import Instagram from "@/assets/img/social/instagram.svg";
+import Linkedin from "@/assets/img/social/linkedin.svg";
+
+export default {
+  name: "Footer",
+  components: {
+    Facebook,
+    Instagram,
+    Linkedin,
+  },
+};
+</script>
+
